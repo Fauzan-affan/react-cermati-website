ed29b75a986b2d7040f4b8fcf9af810c0069ee48
Fauzan-affan <fauzanaffanzaki93@gmail.com>
2020-05-15 22:57:05 +0700

    without animation

---
+++ src/components/Newsletter.js
@@ -0,0 +1,29 @@
+import React, { Fragment } from 'react';
+
+const Newsletter = ({buttonNews, handleButtonNews}) => {
+    return (
+        <Fragment>
+            { buttonNews ? null :
+            <div className="newsletter absolute bottom-0 left-0 z-10">
+                <nav className="flex justify-end items-end w-full">
+                    <button type="button" className="text-white pr-2" onClick={handleButtonNews}>x</button>
+                </nav>
+                <div className="pb-6 pt-2 px-5">
+                    <p className="Headline text-2xl text-white font-bold">Get latest updates in web technologies.</p>
+                    <nav className="pragraph text-white">
+                        I write articles related to web technologies, such as design trends, development
+                        tools, UI/UX case studies and reviews, and more. Sign up to my newsletter to get
+                        them all.
+                    </nav>
+                    <form className="w-full md:flex pt-5">
+                        <input type="text" className="w-full md:w-4/5 p-2 focus:outline-none mb-4" placeholder="Email Address"/>
+                        <button type="submit" className="bg-secondary w-full h-10 md:w-1/5 text-white font-bold py-0 md:ml-4 bg-submit hover:bg-orange-600 transition duration-500 ease-in-out">Count me in!</button>
+                    </form>
+                </div>
+            </div>
+            }
+        </Fragment>
+    );
+}
+
+export default Newsletter;
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      width: {
+        '3/7': '30%',
+      },
+      backgroundColor: theme => ({
+         'primary': '​#007bc1',
+         'secondary': '#ff8000',
+        })
+    },
+  },
+  variants: {},
+  plugins: [],
+}
